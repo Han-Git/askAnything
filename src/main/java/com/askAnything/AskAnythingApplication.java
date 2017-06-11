2d56c0bd87847a15d3dbd7590d4b8353968a6cbc
Han-Git <topofstar0@gmail.com>
2017-06-11 12:17:55 -0500

    H2 database접속시 no table localhost:8080/console/ JDBC URL-> jdbc:h2:~/askAnything
    접속시 테이블이 없음.

--- src/main/java/com/askAnything/AskAnythingApplication.java
+++ src/main/java/com/askAnything/AskAnythingApplication.java
@@ -1,7 +1,10 @@
 package com.askAnything;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AskAnythingApplication {
@@ -9,4 +12,13 @@ public class AskAnythingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AskAnythingApplication.class, args);
 	}
+	
+
+	  @Bean
+	  public ServletRegistrationBean h2servletRegistration() {
+	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+	    registration.addUrlMappings("/console/*");
+
+	    return registration;
+	  }
 }
---
+++ src/main/java/com/askAnything/domain/User.java
@@ -0,0 +1,41 @@
+package com.askAnything.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+  
+  @Id
+  @GeneratedValue
+  private Long id;
+  @Column(nullable = false, length = 20)
+  private String userId;
+  @Column(nullable = false, length = 20)
+  private String password;
+  @Column(nullable = false, length = 20)
+  private String name;
+  @Column(nullable = false, length = 20)
+  private String email;
+  
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  
+  @Override
+  public String toString() {
+    return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email="
+        + email + "]";
+  }
+}
